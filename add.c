3be03864dc57f06c36774cad9f44ddd98e7e0054
Sujitha R <sujitha.r@s.amity.edu>
2025-07-28 12:09:27 +0530

    C11

--- add.c
+++ add.c
@@ -2,3 +2,8 @@
 
 int main() {
     char name[50];
+    printf("========== Welcome Program ==========\n");
+    printf("Hello, World!\n");
+
+    printf("What's your name? ");
+    scanf("%[^\n]", name);
